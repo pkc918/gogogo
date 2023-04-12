774808b771bdda4b1a76dfda7ebc85e853a7d8b6
XiaoPan <2489964425@qq.com>
2023-04-12 12:49:51 +0800

    feat:add stack properties

--- stack/stack.go
+++ stack/stack.go
@@ -38,6 +38,37 @@ func (stack *Stack) Pop() (ele Element, err error){
 		return nil, errors.New("the stack is empty")
 	}
 	ele = stack.elements[stack.top]
+	// 在栈中清除当前元素
+	stack.elements = append(stack.elements, stack.elements[:stack.top], stack.elements[stack.top+1:])
 	stack.top--
 	return ele, nil
+}
+
+// Peek
+func (stack *Stack) Peek() (ele Element, err error){
+	if stack.top <= 0 {
+		return nil, errors.New("the stack is empty")
+	}
+	ele = stack.elements[stack.top]
+	return ele, nil
+}
+
+// Len
+func (stack *Stack) Len() int{
+	return stack.top
+}
+
+// Cap
+func (stack *Stack) Cap() int{
+	return stack.cap
+}
+
+// Clear
+func (stack *Stack) Clear() {
+	if stack.top <= 0 {
+		return
+	}
+	// 重新分配一个空切片
+	stack.elements = stack.elements[:0]
+	stack.top = 0
 }
